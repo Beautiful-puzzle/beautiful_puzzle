39f03ed46cebe9dcbe783bc1e19c51ba3a9ac8a4
KhudoliiAndrew <khudolii.a.s@gmail.com>
2022-01-16 18:22:10 +0200

    feat: add analysis options; add base game field.

---
+++ lib/ui/app.dart
@@ -0,0 +1,17 @@
+import 'package:beautiful_puzzle/ui/game/game_field.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const GameFieldWidget(),
+    );
+  }
+}
